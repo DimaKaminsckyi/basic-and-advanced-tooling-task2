bda806ca2d002f7873f21013a5799a3ba880046a
DimaKaminsckyi <dimakaminsckyi@gmail.com>
2019-02-15 00:13:22 +0200

    add module and tasks

--- src/main/java/com/basecamp/Main.java
+++ src/main/java/com/basecamp/Main.java
@@ -1,7 +1,11 @@
 package com.basecamp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static Logger log = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        log.info("Hello world!");
     }
 }
